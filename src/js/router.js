c4098e2cd8ed0c288d05d4a02e949606115bdd73
Kittypoda <odakrolstad@gmail.com>
2024-12-14 21:33:16 +0100

    fixed path in router

--- src/js/router.js
+++ src/js/router.js
@@ -4,47 +4,49 @@ export default async function router(pathname = window.location.pathname) {
   showLoader();
 
   try {
-  switch (pathname) {
-    case '/Bidzy-Semester-project/':
-    case '/':
-      await import('./view/header');
-      await import('./api/listings/allListings');
-      await import('./api/listings/searchListing.js');
-      await import('../js/api/ui/searchForm.js');
-      await import('../js/api/ui/createListing.js')
-      break;
-    case '/Bidzy-Semester-project/src/html/register.html':
-      await import('./api/ui/register.js');
-      break;
-    case '/Bidzy-Semester-project/src/html/productpage.html':
-      await import('./view/header')
-      await import('../js/api/listings/oneListing.js');
-      await import('./api/listings/searchListing.js');
-      await import('../js/api/ui/searchForm.js');
-      break;
-    case '/Bidzy-Semester-project/src/html/login.html':
-      await import('./api/ui/login.js');
-      break;
-      case '/Bidzy-Semester-project/src/html/search.html':
+    switch (pathname) {
+      case '/Bidzy-Semester-project/':
+      case '/':
+        await import('./view/header');
+        await import('./api/listings/allListings');
+        await import('./api/listings/searchListing.js');
+        await import('../js/api/ui/searchForm.js');
+        await import('../js/api/ui/createListing.js');
+        break;
+      case '/Bidzy-Semester-project/html/register.html':
+        await import('./api/ui/register.js');
+        break;
+      case '/Bidzy-Semester-project/html/productpage.html':
+        await import('./view/header');
+        await import('../js/api/listings/oneListing.js');
+        await import('./api/listings/searchListing.js');
+        await import('../js/api/ui/searchForm.js');
+        break;
+      case '/Bidzy-Semester-project/html/login.html':
+        await import('./api/ui/login.js');
+        break;
+      case '/Bidzy-Semester-project/html/search.html':
         await import('./view/header');
         await import('./api/listings/searchListing.js');
         await import('../js/api/ui/searchForm.js');
         break;
-      case '/Bidzy-Semester-project/src/html/profile.html':
-          await import('./view/header');
-          await import('./api/listings/searchListing.js');
-          await import('../js/api/ui/searchForm.js'); 
-          await import('../js/api/ui/editProfile.js');  
-          await import('./api/ui/createListing.js');     
-          break;
-      case '/Bidzy-Semester-project/src/html/mylistings.html':
+      case '/Bidzy-Semester-project/html/profile.html':
+        await import('./view/header');
+        await import('./api/listings/searchListing.js');
+        await import('../js/api/ui/searchForm.js');
+        await import('../js/api/ui/editProfile.js');
+        await import('./api/ui/createListing.js');
+        break;
+      case '/Bidzy-Semester-project/html/mylistings.html':
         await import('./view/header');
         await import('../js/api/listings/showMyListings.js');
-
-        }
-      } catch (error) {
-        console.error(`Error loading route: ${pathname}`, error);
-      } finally {
-        hideLoader(); // Hide loader after content has loaded
-      }
+        break;
+      default:
+        console.error(`No matching route for: ${pathname}`);
     }
+  } catch (error) {
+    console.error(`Error loading route: ${pathname}`, error);
+  } finally {
+    hideLoader(); // Hide loader after content has loaded
+  }
+}
