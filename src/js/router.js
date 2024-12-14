9b5410f16d856e16d843d28060da30ef217febf8
Kittypoda <odakrolstad@gmail.com>
2024-12-14 19:37:26 +0100

    is it fixed now?

--- src/js/router.js
+++ src/js/router.js
@@ -13,31 +13,31 @@ export default async function router(pathname = window.location.pathname) {
       await import('../js/api/ui/searchForm.js');
       await import('../js/api/ui/createListing.js')
       break;
-    case '/Bidzy-Semester-project/src/html/register.html':
+    case '/Bidzy-Semester-project/src/html/register.html/':
       await import('./api/ui/register.js');
       break;
-    case '/Bidzy-Semester-project/src/html/productpage.html':
+    case '/Bidzy-Semester-project/src/html/productpage.html/':
       await import('./view/header')
       await import('../js/api/listings/oneListing.js');
       await import('./api/listings/searchListing.js');
       await import('../js/api/ui/searchForm.js');
       break;
-    case '/Bidzy-Semester-project/src/html/login.html':
+    case '/Bidzy-Semester-project/src/html/login.html/':
       await import('./api/ui/login.js');
       break;
-      case '/Bidzy-Semester-project/src/html/search.html':
+      case '/Bidzy-Semester-project/src/html/search.html/':
         await import('./view/header');
         await import('./api/listings/searchListing.js');
         await import('../js/api/ui/searchForm.js');
         break;
-      case '/Bidzy-Semester-project/src/html/profile.html':
+      case '/Bidzy-Semester-project/src/html/profile.html/':
           await import('./view/header');
           await import('./api/listings/searchListing.js');
           await import('../js/api/ui/searchForm.js'); 
           await import('../js/api/ui/editProfile.js');  
           await import('./api/ui/createListing.js');     
           break;
-      case '/Bidzy-Semester-project/src/html/mylistings.html':
+      case '/Bidzy-Semester-project/src/html/mylistings.html/':
         await import('./view/header');
         await import('../js/api/listings/showMyListings.js');
 
